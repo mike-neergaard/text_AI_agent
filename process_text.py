b0c8f0d2e4bae61cb686caaf8a243be662ebb32f
dad <mike.neergaard@gmail.com>
2023-12-03 14:50:29 +0100

    chunk processing working

--- process_text.py
+++ process_text.py
@@ -5,7 +5,7 @@ import spacy
 import json
 
 class chunk_utils:
-    """A class of chunk-managing utilities"""
+    """A class of text chunk-managing utilities"""
     current_chunk = "" 
     current_chunk_tokens = 0
     line_number = 0
@@ -18,7 +18,7 @@ class chunk_utils:
         self.tokenizer = AutoTokenizer.from_pretrained(models["text_tokenizer"])
         # spacy seems to give better results in this context than langchain 
         self.s_nlp = spacy.load('en_core_web_trf')
-        self.max_tokens = tokenizer.model_max_length
+        self.max_tokens = self.tokenizer.model_max_length
 
         # Technically min_tokens can be a float, but this feels cleaner
         self.min_tokens = int(self.min_token_percentage * self.max_tokens + 0.5)
@@ -27,12 +27,12 @@ class chunk_utils:
         #self.line_splitter = RecursiveCharacterTextSplitter( 
         #        chunk_size = 100, #overwrite
         #        chunk_overlap = 20,
-        #        length_function = count_tokens)
+        #        length_function = self.count_tokens)
 
 
-    def count_tokens(s: str) -> int:
+    def count_tokens(self, s: str) -> int:
         """Returns the number of tokens in a text string."""
-        tokens = tokenizer(s)
+        tokens = self.tokenizer(s)
         return len(tokens["input_ids"])
 
     def reset_current_chunk(self):
@@ -41,27 +41,31 @@ class chunk_utils:
 
     def add_line_to_chunk(self,line: str):
         self.current_chunk += line
-        self.current_chunk_tokens = count_tokens(self.current_chunk)
+        self.current_chunk_tokens = self.count_tokens(self.current_chunk)
 
     def new_chunk(self, line: str):
         self.current_chunk = line
-        self.current_chunk_tokens = count_tokens(self.current_chunk)
+        self.current_chunk_tokens = self.count_tokens(self.current_chunk)
 
     def dump_chunk(self, lines): 
-        lines.append({"text":current_chunk, 
-            "len": current_chunk_tokens, 
+        lines.append({"text":self.current_chunk, 
+            "len": self.current_chunk_tokens, 
             "line number": self.line_number}) 
         self.line_number += 1 
         self.reset_current_chunk()
 
     def chunk_line(self, line: str, num_tokens: int, 
-            margin: int = int(self.max_tokens * 0.10)) -> list: 
+            margin: int = -999) -> list: 
+
+        # Can't pass class variable as a parameter, so this is the workaround
+        if margin == -999:
+            margin = int(self.max_tokens * 0.10)
 
         # Let spacy turn this line into sentences
         s_line = self.s_nlp(line)
         sentences = [str(i) for i in s_line.sents]
     
-        num_chunks = int(num_tokens / max_tokens) + 1
+        num_chunks = int(num_tokens / self.max_tokens) + 1
         # Give a margin for the maximum chunk size
         # But not larger than max_tokens
         chunk_size = min(int(num_tokens / num_chunks) + margin, self.max_tokens)
@@ -71,20 +75,20 @@ class chunk_utils:
         for i in range(len(sentences)): 
             # Extend the current chunk by one sentence
             new_chunk = current_chunk + sentences[i]
-            if count_tokens(new_chunk) > chunk_size:
+            if self.count_tokens(new_chunk) > chunk_size:
                 # Time to start a new chunk
-                chunks.append([current_chunk, count_tokens(current_chunk)])
+                chunks.append([current_chunk, self.count_tokens(current_chunk)])
                 current_chunk = sentences[i]
             else:
                 # Update current chunk and continue
                 current_chunk = new_chunk
     
-        chunks.append([current_chunk, count_tokens(current_chunk)])
+        chunks.append([current_chunk, self.count_tokens(current_chunk)])
     
         return chunks
     
-    def add_line(self, line: str, lines: list):
-        """ If we get to this point, we have to write out a line"""
+    def record_chunk(self, line: str, lines: list):
+        """ If we get to this point, we have to write out a chunk"""
     
         # If there is a current chunk, write it out first.
         if self.current_chunk_tokens: 
@@ -93,10 +97,10 @@ class chunk_utils:
         # If the line is empty, we are done here
         if line=="": return
     
-        line_tokens = count_tokens(line)
+        line_tokens = self.count_tokens(line)
         if line_tokens > self.max_tokens:
             # This line is itself too long, so we will have to chunk it 
-            chunks=chunk_line(line, line_tokens)
+            chunks=self.chunk_line(line, line_tokens)
             # Write the chunks out right away
             for c in chunks:
                 lines.append({"text":c[0], "len": c[1], 
@@ -109,6 +113,7 @@ class chunk_utils:
 
 
 def process_text():
+    ch = chunk_utils()
     file_list = [f for f in os.listdir("text") \
            if os.path.isfile(os.path.join("text", f))]
     file_list.sort()
@@ -122,23 +127,23 @@ def process_text():
     
         with open(os.path.join("text", file), "r") as book:
             # We might have to concatenate chunks, so we maintain a current chunk
-            G.reset_current_chunk()
+            ch.reset_current_chunk()
     
             for line in book:
                 # We have to count tokens this way, because the concatenated string
                 # may tokenize differently than the individual strings
-                num_tokens = count_tokens(current_chunk + line)
-                if num_tokens > min_tokens: 
+                num_tokens = ch.count_tokens(ch.current_chunk + line)
+                if num_tokens > ch.min_tokens: 
                     # With this line, we have exceeded the threshold, so we must
                     # record a chunk
-                    record_chunk(line, lines)
+                    ch.record_chunk(line, lines)
                 else:
                     # Even with this line added, we have not exceeded the threshold
-                    G.add_line_to_chunk(line)
+                    ch.add_line_to_chunk(line)
     
             # If there is a leftover chunk, write it out.
-            if G.current_chunk_tokens: 
-                G.dump_chunk(lines)
+            if ch.current_chunk_tokens: 
+                ch.dump_chunk(lines)
         print("Done", flush=True)
 
     with open(os.path.join("data", "lines.json"), "w") as outfile:
